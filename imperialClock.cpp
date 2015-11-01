2981ffac202532435a7a172951819bb3967baf4c
nholthaus <nholthaus@gmail.com>
2015-11-01 10:32:12 -0500

    sleanup to support vs2013 build

--- imperialClock.cpp
+++ imperialClock.cpp
@@ -1,6 +1,326 @@
+#include "imperialClock.h"
 
+//------------------------------------------------------------------------------
+//	FUNCTION: loadSettings [ ]
+//------------------------------------------------------------------------------
 void imperialClock::loadSettings()
 {
-	throw std::logic_error("The method or operation is not implemented.");
+	QSettings settings("Logitech", "Logitech Gaming Software", this);
+	settings.beginGroup("LCDImperialClock");
+	{
+		smoothCheckBox->setChecked(settings.value("smoothMovement", true).toBool());
+		dateCheckBox->setChecked(settings.value("dateEnabled", true).toBool());
+		timeCheckBox->setChecked(settings.value("timeEnabled", true).toBool());
+		twentyFourHourTimeCheckBox->setChecked(settings.value("twentyFourHourTime", false).toBool());
+		personalizedMessageEdit->setText(settings.value("personalizedMessage", "").toString());
+	}
+	settings.endGroup();
 }
 
+//------------------------------------------------------------------------------
+//	FUNCTION: saveSettings [ private ]
+//------------------------------------------------------------------------------
+void imperialClock::saveSettings()
+{
+	QSettings settings("Logitech", "Logitech Gaming Software", this);
+	settings.beginGroup("LCDImperialClock");
+	{
+		settings.setValue("smoothMovement", smoothCheckBox->isChecked());
+		settings.setValue("dateEnabled", dateCheckBox->isChecked());
+		settings.setValue("timeEnabled", timeCheckBox->isChecked());
+		settings.setValue("twentyFourHourTime", twentyFourHourTimeCheckBox->isChecked());
+		settings.setValue("personalizedMessage", personalizedMessageEdit->text());
+	}
+	settings.endGroup();
+
+	qApp->quit();
+}
+
+//------------------------------------------------------------------------------
+//	FUNCTION: createGuiElements [ ]
+//------------------------------------------------------------------------------
+void imperialClock::createGuiElements()
+{
+	this->setLayout(new QHBoxLayout);
+
+	optionsGroupBox = new QGroupBox("Options");
+
+	this->layout()->addWidget(optionsGroupBox);
+
+	smoothCheckBox = new QCheckBox("Smooth Movement (uses more CPU)");
+	dateCheckBox = new QCheckBox("Show Date");
+	timeCheckBox = new QCheckBox("Show Digital Time");
+	twentyFourHourTimeCheckBox = new QCheckBox("24 Hour Time");
+
+	connect(smoothCheckBox, &QCheckBox::toggled, [&](bool checked){ checked ? timer->setInterval(50) : timer->setInterval(1000); });
+	connect(dateCheckBox, &QCheckBox::toggled, [&](bool checked){checked ? dateEnabled = true : dateEnabled = false; });
+	connect(timeCheckBox, &QCheckBox::toggled, [&](bool checked){checked ? timeEnabled = true : timeEnabled = false; });
+
+	smoothCheckBox->setToolTip("Smoothes the motion of the second hand sweep at the expense of some additional CPU processing");
+	dateCheckBox->setToolTip("This can also be toggled by holding down the 'UP' button on the Logitech G19s D-pad.");
+	timeCheckBox->setToolTip("This can also be toggled by holding down the 'DOWN' button on the Logitech G19s D-pad.");
+	twentyFourHourTimeCheckBox->setToolTip("Toggles between a 12 and 24 hour clock when Digital time is enabled.");
+
+	smoothCheckBox->setChecked(true);
+	dateCheckBox->setChecked(true);
+	timeCheckBox->setChecked(true);
+	twentyFourHourTimeCheckBox->setChecked(false);
+
+	personalizedMessageEdit = new QLineEdit(this);
+	personalizedMessageEdit->setPlaceholderText("Your Personalized Message Here...");
+	personalizedMessageEdit->setMaxLength(29);
+
+	QVBoxLayout* layout = new QVBoxLayout;
+
+	layout->addWidget(smoothCheckBox);
+	layout->addWidget(dateCheckBox);
+	layout->addWidget(timeCheckBox);
+	layout->addWidget(twentyFourHourTimeCheckBox);
+	layout->addWidget(personalizedMessageEdit);
+
+	optionsGroupBox->setLayout(layout);
+
+	// menu
+	trayIconMenu = new QMenu(this);
+
+	minimizeAction = new QAction("Minimize", trayIconMenu);
+	restoreAction = new QAction("Restore", trayIconMenu);
+	quitAction = new QAction("Exit", trayIconMenu);
+
+	trayIconMenu->addAction(minimizeAction);
+	trayIconMenu->addAction(restoreAction);
+	trayIconMenu->addSeparator();
+	trayIconMenu->addAction(quitAction);
+
+	connect(minimizeAction, &QAction::triggered, this, std::bind(&QDialog::setVisible, this, false));
+	connect(restoreAction, &QAction::triggered, this, std::bind(&QDialog::setVisible, this, true));
+	connect(quitAction, &QAction::triggered, this, &imperialClock::saveSettings);
+
+	// system tray
+	QIcon icon(":/icon");
+	trayIcon = new QSystemTrayIcon(icon, this);
+	setWindowIcon(icon);
+
+	connect(trayIcon, &QSystemTrayIcon::activated, [&](QSystemTrayIcon::ActivationReason reason)
+	{
+		if (reason == QSystemTrayIcon::Trigger)
+		{
+			this->setVisible(true);
+		}
+	});
+
+	trayIcon->setToolTip("Logitech G19s Imperial Clock Applet");
+	trayIcon->setContextMenu(trayIconMenu);
+	trayIcon->show();
+}
+
+//------------------------------------------------------------------------------
+//	FUNCTION: setupClockHands [ ]
+//------------------------------------------------------------------------------
+void imperialClock::setupClockHands()
+{
+	secondHand[0] = QPoint(0, 10);
+	secondHand[1] = QPoint(1, 0);
+	secondHand[2] = QPoint(0, -95);
+	secondHand[3] = QPoint(-1, 0);
+
+	minuteHand[0] = QPoint(0, 10);
+	minuteHand[1] = QPoint(2, 0);
+	minuteHand[2] = QPoint(0, -95);
+	minuteHand[3] = QPoint(-2, 0);
+
+	hourHand[0] = QPoint(0, 10);
+	hourHand[1] = QPoint(4, 0);
+	hourHand[2] = QPoint(0, -50);
+	hourHand[3] = QPoint(-4, 0);
+}
+
+//------------------------------------------------------------------------------
+//	FUNCTION: drawClock [ ]
+//------------------------------------------------------------------------------
+void imperialClock::drawClock()
+{
+	bool buttonUp = LogiLcdIsButtonPressed(LOGI_LCD_COLOR_BUTTON_UP);
+	bool buttonDown = LogiLcdIsButtonPressed(LOGI_LCD_COLOR_BUTTON_DOWN);
+
+	if (buttonUp && !prevButtonUp)
+	{
+		dateCheckBox->toggle();
+	}
+	if (buttonDown && !prevButtonDown)
+	{
+		timeCheckBox->toggle();
+	}
+
+	prevButtonDown = buttonDown;
+	prevButtonUp = buttonUp;
+
+	using namespace std;
+	using namespace std::chrono;
+
+	QDateTime now = QDateTime::currentDateTime();
+
+	int hour = now.time().hour();
+	if (!twentyFourHourTimeCheckBox->isChecked())
+	{
+		hour = hour % 12;
+	}
+	int minute = now.time().minute();
+	int second = now.time().second();
+	int msec = now.time().msec();
+
+	QImage clock(*logo);
+
+	QPainter p(&clock);
+	p.setRenderHints(QPainter::HighQualityAntialiasing | QPainter::SmoothPixmapTransform);
+	p.translate(clock.width() / 2, clock.height() / 2);
+
+	p.setPen(Qt::NoPen);
+	p.setBrush(Qt::white);
+
+	// draw hours
+	p.save();
+	p.setBrush(QColor("#BBBBBB"));
+	p.rotate(30.0 * ((hour + minute / 60.0)));
+	p.drawConvexPolygon(hourHand, 4);
+	p.restore();
+
+	// draw minutes
+	p.save();
+	p.setBrush(QColor("#999999"));
+	p.rotate(6.0 * ((minute + second / 60.0)));
+	p.drawConvexPolygon(minuteHand, 4);
+	p.restore();
+
+	// draw seconds
+	p.save();
+	p.setBrush(Qt::white);
+	p.rotate(6.0 * (second + msec / 1000.0));
+	p.drawConvexPolygon(secondHand, 4);
+	p.restore();
+
+	p.setBrush(Qt::black);
+	p.drawEllipse(QPoint(0, 0), 3, 3);
+	p.end();
+
+	clock.convertToFormat(QImage::Format_RGB32);
+
+	LogiLcdColorSetBackground(clock.bits());
+
+	if (dateEnabled)
+	{
+		std::stringstream date;
+		date << ' ' << now.date().toString().toStdString();
+		LogiLcdColorSetTitle(to_wchar_t(date.str().c_str()), 255, 255, 255);
+	}
+	else
+	{
+		LogiLcdColorSetTitle(to_wchar_t(""), 255, 255, 255);
+	}
+
+	if (!personalizedMessageEdit->text().isEmpty())
+	{
+		std::stringstream date;
+		date << personalizedMessageEdit->text().toStdString();
+		LogiLcdColorSetText(6, to_wchar_t(date.str().c_str()), 255, 255, 255);
+	}
+	else
+	{
+		LogiLcdColorSetText(6, to_wchar_t(""), 255, 255, 255);
+	}
+
+	if (timeEnabled)
+	{
+		std::stringstream time;
+		time << "           " << hour << ':' << std::setfill('0') << std::setw(2) << minute << ':' << std::setw(2) << second;
+		LogiLcdColorSetText(7, to_wchar_t(time.str().c_str()), 255, 255, 255);
+	}
+	else
+	{
+		LogiLcdColorSetText(7, to_wchar_t(""), 255, 255, 255);
+	}
+
+	LogiLcdUpdate();
+}
+
+//------------------------------------------------------------------------------
+//	FUNCTION: startClock [ ]
+//------------------------------------------------------------------------------
+void imperialClock::startClock()
+{
+	connect(timer, &QTimer::timeout, this, &imperialClock::drawClock);
+	timer->start(50);
+}
+
+//------------------------------------------------------------------------------
+//	FUNCTION: initializeLcd [ ]
+//------------------------------------------------------------------------------
+void imperialClock::initializeLcd()
+{
+	LogiLcdInit(to_wchar_t("Imperial Clock"), LOGI_LCD_TYPE_COLOR);
+
+	logo->convertToFormat(QImage::Format_RGB32);
+
+	LogiLcdColorSetBackground(logo->bits());
+	LogiLcdUpdate();
+}
+
+//------------------------------------------------------------------------------
+//	FUNCTION: closeEvent [ ]
+//------------------------------------------------------------------------------
+void imperialClock::closeEvent(QCloseEvent *event)
+{
+	if (trayIcon->isVisible()) {
+		trayIcon->showMessage(tr("Logitech G19s Imperial Clock"),
+			tr("This program will continue to run in the "
+			"background. To terminate the program, "
+			"choose 'Quit' in the context menu "
+			"of the system tray icon."));
+		hide();
+		event->ignore();
+	}
+}
+
+//------------------------------------------------------------------------------
+//	FUNCTION: setVisible [ ]
+//------------------------------------------------------------------------------
+void imperialClock::setVisible(bool visible)
+{
+	minimizeAction->setEnabled(visible);
+	restoreAction->setEnabled(!visible);
+	QDialog::setVisible(visible);
+}
+
+//------------------------------------------------------------------------------
+//	FUNCTION: ~imperialClock [ ]
+//------------------------------------------------------------------------------
+imperialClock::~imperialClock()
+{
+	LogiLcdShutdown();
+}
+
+//------------------------------------------------------------------------------
+//	FUNCTION: imperialClock [ ]
+//------------------------------------------------------------------------------
+imperialClock::imperialClock() : QDialog(),
+logo(new QImage(":/logo")),
+timer(new QTimer)
+{
+	setupClockHands();
+	initializeLcd();
+	createGuiElements();
+	loadSettings();
+	startClock();
+}
+
+//------------------------------------------------------------------------------
+//	FUNCTION: to_wchar_t [ private ]
+//------------------------------------------------------------------------------
+wchar_t* imperialClock::to_wchar_t(const char* string)
+{
+	size_t newsize = strlen(string) + 1;
+	wchar_t * wText = new wchar_t[newsize];
+	size_t convertedChars = 0;
+	mbstowcs_s(&convertedChars, wText, newsize, string, _TRUNCATE);
+	return wText;
+}
--- imperialClock.h
+++ imperialClock.h
@@ -1,3 +1,42 @@
+//--------------------------------------------------------------------------------------------------
+// 
+/// @PROJECT	imperialClock
+///	@AUTHORS	Nic Holthaus
+/// @DATE		2014/12/25
+//
+//--------------------------------------------------------------------------------------------------
+//
+// The MIT License (MIT)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+// and associated documentation files (the "Software"), to deal in the Software without 
+// restriction, including without limitation the rights to use, copy, modify, merge, publish, 
+// distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+// Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or 
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//--------------------------------------------------------------------------------------------------
+// 
+// Copyright (c) 2014 Nic Holthaus
+// 
+//--------------------------------------------------------------------------------------------------
+
+#ifndef imperialClock_h__
+#define imperialClock_h__
+
+//--------------------
+//	INCLUDES
+//--------------------
+
+// Qt
 #include <QDialog>
 #include <cstdlib>
 #include <QTimer>
@@ -16,6 +55,7 @@
 #include <QLineEdit>
 #include <QSettings>
 
+// std
 #include <chrono>
 #include <sstream>
 #include <ctime>
@@ -23,49 +63,26 @@
 #include <functional>
 #include <iomanip>
 
+// local
 #include "LogitechLCDLib.h"
 
+//--------------------------------------------------------------------------------
+///	@class		imperialClock
+//--------------------------------------------------------------------------------
+/// @brief		main object for the imperialClock project
+/// @details
+//--------------------------------------------------------------------------------
 class imperialClock : public QDialog
 {
 	Q_OBJECT
 
 public:
-	imperialClock() : QDialog(),
-	logo(new QImage(":/logo")),
-	timer(new QTimer)
-	{
-		setupClockHands();
-		initializeLcd();
-		createGuiElements();
-		loadSettings();
-		startClock();
 
-	}
+	explicit imperialClock();
+	virtual ~imperialClock();
 
-	~imperialClock()
-	{
-		LogiLcdShutdown();
-	}
-
-	void setVisible(bool visible)
-	{
-		minimizeAction->setEnabled(visible);
-		restoreAction->setEnabled(!visible);
-		QDialog::setVisible(visible);
-	}
-
-	void closeEvent(QCloseEvent *event)
-	{
-		if (trayIcon->isVisible()) {
-			trayIcon->showMessage(tr("Logitech G19s Imperial Clock"),
-				tr("This program will continue to run in the "
-				"background. To terminate the program, "
-				"choose 'Quit' in the context menu "
-				"of the system tray icon."));
-			hide();
-			event->ignore();
-		}
-	}
+	void setVisible(bool visible);
+	void closeEvent(QCloseEvent *event) override;
 
 protected:
 
@@ -86,259 +103,14 @@ protected:
 
 private:
 
-	wchar_t* to_wchar_t(const char* string)
-	{
-		size_t newsize = strlen(string) + 1;
-		wchar_t * wText = new wchar_t[newsize];
-		size_t convertedChars = 0;
-		mbstowcs_s(&convertedChars, wText, newsize, string, _TRUNCATE);
-		return wText;
-	}
-
-	void initializeLcd()
-	{
-		LogiLcdInit(to_wchar_t("Imperial Clock"), LOGI_LCD_TYPE_COLOR);
-
-		logo->convertToFormat(QImage::Format_RGB32);
-
-		LogiLcdColorSetBackground(logo->bits());
-		LogiLcdUpdate();
-	}
-
-	void startClock()
-	{
-		connect(timer, &QTimer::timeout, this, &imperialClock::drawClock);
-		timer->start(50);
-	}
-
-
-	void drawClock()
-	{
-		bool buttonUp = LogiLcdIsButtonPressed(LOGI_LCD_COLOR_BUTTON_UP);
-		bool buttonDown = LogiLcdIsButtonPressed(LOGI_LCD_COLOR_BUTTON_DOWN);
-		
-		if (buttonUp && !prevButtonUp)
-		{
-			dateCheckBox->toggle();
-		}
-		if (buttonDown && !prevButtonDown)
-		{
-			timeCheckBox->toggle();
-		}
-
-		prevButtonDown = buttonDown;
-		prevButtonUp = buttonUp;
-
-		using namespace std;
-		using namespace std::chrono;
-
-		QDateTime now = QDateTime::currentDateTime();
-
-		int hour = now.time().hour();
-		if (!twentyFourHourTimeCheckBox->isChecked())
-		{
-			hour = hour % 12;
-		}
-		int minute = now.time().minute();
-		int second = now.time().second();
-		int msec = now.time().msec();
-
-		QImage clock(*logo);
-
-		QPainter p(&clock);
-		p.setRenderHints(QPainter::HighQualityAntialiasing | QPainter::SmoothPixmapTransform);
-		p.translate(clock.width() / 2, clock.height() / 2);
-
-		p.setPen(Qt::NoPen);
-		p.setBrush(Qt::white);
-
-		// draw hours
-		p.save();
-		p.setBrush(QColor("#BBBBBB"));
-		p.rotate(30.0 * ((hour + minute / 60.0)));
-		p.drawConvexPolygon(hourHand, 4);
-		p.restore();
-
-		// draw minutes
-		p.save();
-		p.setBrush(QColor("#999999"));
-		p.rotate(6.0 * ((minute + second / 60.0)));
-		p.drawConvexPolygon(minuteHand, 4);
-		p.restore();
-
-		// draw seconds
-		p.save();
-		p.setBrush(Qt::white);
-		p.rotate(6.0 * (second + msec/1000.0));
-		p.drawConvexPolygon(secondHand, 4);
-		p.restore();
-
-		p.setBrush(Qt::black);
-		p.drawEllipse(QPoint(0,0), 3, 3);
-		p.end();
-
-		clock.convertToFormat(QImage::Format_RGB32);
-
-		LogiLcdColorSetBackground(clock.bits());
-
-		if (dateEnabled)
-		{
-			std::stringstream date;
-			date << ' ' << now.date().toString().toStdString();
-			LogiLcdColorSetTitle(to_wchar_t(date.str().c_str()), 255, 255, 255);
-		}
-		else
-		{
-			LogiLcdColorSetTitle(to_wchar_t(""), 255, 255, 255);
-		}
-
-		if (!personalizedMessageEdit->text().isEmpty())
-		{
-			std::stringstream date;
-			date << personalizedMessageEdit->text().toStdString();
-			LogiLcdColorSetText(6, to_wchar_t(date.str().c_str()), 255, 255, 255);
-		}
-		else
-		{
-			LogiLcdColorSetText(6, to_wchar_t(""), 255, 255, 255);
-		}
-
-		if (timeEnabled)
-		{
-			std::stringstream time;
-			time << "           " << hour << ':' << std::setfill('0') << std::setw(2) << minute << ':' << std::setw(2) << second;
-			LogiLcdColorSetText(7, to_wchar_t(time.str().c_str()), 255, 255, 255);
-		}
-		else
-		{
-			LogiLcdColorSetText(7, to_wchar_t(""), 255, 255, 255);
-		}
-
-		LogiLcdUpdate();
-	}
-
-	void setupClockHands()
-	{
-		secondHand[0] = QPoint(0, 10);
-		secondHand[1] = QPoint(1, 0);
-		secondHand[2] = QPoint(0, -95);
-		secondHand[3] = QPoint(-1, 0);
-
-		minuteHand[0] = QPoint(0, 10);
-		minuteHand[1] = QPoint(2,0);
-		minuteHand[2] = QPoint(0, -95);
-		minuteHand[3] = QPoint(-2, 0);
-
-		hourHand[0] = QPoint(0, 10);
-		hourHand[1] = QPoint(4, 0);
-		hourHand[2] = QPoint(0, -50);
-		hourHand[3] = QPoint(-4, 0);
-	}
-
-	void createGuiElements()
-	{
-		this->setLayout(new QHBoxLayout);
-		
-		optionsGroupBox = new QGroupBox("Options");
-
-		this->layout()->addWidget(optionsGroupBox);
-
-		smoothCheckBox = new QCheckBox("Smooth Movement (uses more CPU)");
-		dateCheckBox = new QCheckBox("Show Date");
-		timeCheckBox = new QCheckBox("Show Digital Time");
-		twentyFourHourTimeCheckBox = new QCheckBox("24 Hour Time");
-
-		connect(smoothCheckBox, &QCheckBox::toggled, [&](bool checked){ checked ? timer->setInterval(50) : timer->setInterval(1000); });
-		connect(dateCheckBox, &QCheckBox::toggled, [&](bool checked){checked ? dateEnabled = true : dateEnabled = false; });
-		connect(timeCheckBox, &QCheckBox::toggled, [&](bool checked){checked ? timeEnabled = true : timeEnabled = false; });
-		
-		smoothCheckBox->setToolTip("Smoothes the motion of the second hand sweep at the expense of some additional CPU processing");
-		dateCheckBox->setToolTip("This can also be toggled by holding down the 'UP' button on the Logitech G19s D-pad.");
-		timeCheckBox->setToolTip("This can also be toggled by holding down the 'DOWN' button on the Logitech G19s D-pad.");
-		twentyFourHourTimeCheckBox->setToolTip("Toggles between a 12 and 24 hour clock when Digital time is enabled.");
-
-		smoothCheckBox->setChecked(true);
-		dateCheckBox->setChecked(true);
-		timeCheckBox->setChecked(true);
-		twentyFourHourTimeCheckBox->setChecked(false);
-
-		personalizedMessageEdit = new QLineEdit(this);
-		personalizedMessageEdit->setPlaceholderText("Your Personalized Message Here...");
-		personalizedMessageEdit->setMaxLength(29);
-
-		QVBoxLayout* layout = new QVBoxLayout;
-
-		layout->addWidget(smoothCheckBox);
-		layout->addWidget(dateCheckBox);
-		layout->addWidget(timeCheckBox);
-		layout->addWidget(twentyFourHourTimeCheckBox);
-		layout->addWidget(personalizedMessageEdit);
-
-		optionsGroupBox->setLayout(layout);
-
-		// menu
-		trayIconMenu = new QMenu(this);
-
-		minimizeAction = new QAction("Minimize", trayIconMenu);
-		restoreAction = new QAction("Restore", trayIconMenu);
-		quitAction = new QAction("Exit", trayIconMenu);
-
-		trayIconMenu->addAction(minimizeAction);
-		trayIconMenu->addAction(restoreAction);
-		trayIconMenu->addSeparator();
-		trayIconMenu->addAction(quitAction);
-
-		connect(minimizeAction, &QAction::triggered, this, std::bind(&QDialog::setVisible, this, false));
-		connect(restoreAction, &QAction::triggered, this, std::bind(&QDialog::setVisible, this, true));
-		connect(quitAction, &QAction::triggered, this, &imperialClock::saveSettings);
-
-		// system tray
-		QIcon icon(":/icon");
-		trayIcon = new QSystemTrayIcon(icon, this);
-		setWindowIcon(icon);
-
-		connect(trayIcon, &QSystemTrayIcon::activated, [&](QSystemTrayIcon::ActivationReason reason)
-		{
-			if (reason == QSystemTrayIcon::Trigger)
-			{
-				this->setVisible(true);
-			}
-		});
-
-		trayIcon->setToolTip("Logitech G19s Imperial Clock Applet");
-		trayIcon->setContextMenu(trayIconMenu);
-		trayIcon->show();
-	}
-
-	void loadSettings()
-	{
-		QSettings settings("Logitech", "Logitech Gaming Software", this);
-		settings.beginGroup("LCDImperialClock");
-		{
-			smoothCheckBox->setChecked(settings.value("smoothMovement", true).toBool());
-			dateCheckBox->setChecked(settings.value("dateEnabled", true).toBool());
-			timeCheckBox->setChecked(settings.value("timeEnabled", true).toBool());
-			twentyFourHourTimeCheckBox->setChecked(settings.value("twentyFourHourTime", false).toBool());
-			personalizedMessageEdit->setText(settings.value("personalizedMessage", "").toString());
-		}
-		settings.endGroup();
-	}
-
-	void saveSettings()
-	{
-		QSettings settings("Logitech", "Logitech Gaming Software", this);
-		settings.beginGroup("LCDImperialClock");
-		{
-			settings.setValue("smoothMovement", smoothCheckBox->isChecked());
-			settings.setValue("dateEnabled", dateCheckBox->isChecked());
-			settings.setValue("timeEnabled", timeCheckBox->isChecked());
-			settings.setValue("twentyFourHourTime", twentyFourHourTimeCheckBox->isChecked());
-			settings.setValue("personalizedMessage", personalizedMessageEdit->text());
-		}
-		settings.endGroup();
-
-		qApp->quit();
-	}
+	wchar_t* to_wchar_t(const char* string);
+	void initializeLcd();
+	void startClock();
+	void drawClock();
+	void setupClockHands();
+	void createGuiElements();
+	void loadSettings();
+	void saveSettings();
 
 private:
 
@@ -355,3 +127,5 @@ private:
 	bool prevButtonUp;
 	bool prevButtonDown;
 };
+
+#endif // imperialClock_h__
